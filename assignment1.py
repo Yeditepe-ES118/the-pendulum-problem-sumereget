03a318daf1859dd5bedc957811ef7daf8faa9aaa
sumereget <sumerege.tunca@std.yeditepe.edu.tr>
2025-11-02 19:51:50 +0300

    assignment 1 solutions

--- assignment1.py
+++ assignment1.py
@@ -5,7 +5,7 @@ def find_period(L0,L1):
    
        g = 9.81 #m/s^2
        T = 2 * np.pi * np.sqrt(L/g) #in seconds
-       print(" When L = %1.1f m, T = %2.1f s " % (L,T))
+       print(" When L = %4.1f m, T = %2.1f s" % (L,T))
 
    T0 = 2 * np.pi * np.sqrt(L0/g) #in s
    T1 = 2 * np.pi * np.sqrt(L1/g) #in s
